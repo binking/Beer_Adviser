bdfed2bd857e82a217af62cf6e4b1149f5d196f0
江智彬 <jiangzhibin2014.xujie@gmail.com>
2016-05-07 11:12:57 +0800

    This is my first interactive android app, taking an example in Head First Android as example.

---
+++ BeerAdviser/app/src/main/java/om/binking/beeradviser/BeerExpert.java
@@ -0,0 +1,25 @@
+package om.binking.beeradviser;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by BinKing on 2016/5/7.
+ */
+public class BeerExpert {
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<String>();
+        if (color.equals("Deep")) {
+            brands.add("Deep Blue");
+            brands.add("Deep Sea");
+        } else if (color.equals("Light")){
+            brands.add("Light Speed");
+            brands.add("Light Thunder");
+        }else if (color.equals("Green")){
+            brands.add("Green Leaf");
+            brands.add("Green Tea");
+        }else{
+            brands.add("Blue Love");
+            brands.add("Blue Water");
+        }
+        return brands;
+    }
+}
