3df7ac144f77bd6e33b25c9e34dde0b73531b0e0
江智彬 <jiangzhibin2014.xujie@gmail.com>
2016-05-07 11:31:02 +0800

    Change another repositrory

--- BeerAdviser/app/src/main/java/om/binking/beeradviser/BeerExpert.java
+++ BeerAdviser/app/src/main/java/om/binking/beeradviser/BeerExpert.java
@@ -8,8 +8,8 @@ public class BeerExpert {
     List<String> getBrands(String color) {
         List<String> brands = new ArrayList<String>();
         if (color.equals("Deep")) {
-            brands.add("Deep Blue");
-            brands.add("Deep Sea");
+            brands.add("深蓝");
+            brands.add("深海");
         } else if (color.equals("Light")){
             brands.add("Light Speed");
             brands.add("Light Thunder");
